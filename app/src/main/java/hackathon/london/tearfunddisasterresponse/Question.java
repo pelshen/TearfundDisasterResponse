dbbfa57add49a3da39577c6a26539d519608508b
Sam Thomas <sthomas.gb@gmail.com>
2015-10-03 15:05:49 +0100

    Basic home page plus some classes: -item report - for storing report data -question - to define a question with image and answers -questions - to associate questions with categories

---
+++ app/src/main/java/hackathon/london/tearfunddisasterresponse/Question.java
@@ -0,0 +1,30 @@
+package hackathon.london.tearfunddisasterresponse;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sam on 03/10/15.
+ */
+public class Question {
+
+    private String question = null;
+    private Bitmap illustration = null;
+    private ArrayList<String> answers = new ArrayList<String>();
+
+    public Question(String question) {
+        this.question = question;
+    }
+
+    public Question(String question, ArrayList<String> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public Question(String question, Bitmap illustration, ArrayList<String> answers) {
+        this.question = question;
+        this.illustration = illustration;
+        this.answers = answers;
+    }
+}
