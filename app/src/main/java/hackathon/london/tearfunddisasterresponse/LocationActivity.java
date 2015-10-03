2245d7fa6ea5c093722f1e5ed4f6d2663ba9c449
Edward Bennigsen <pe.ads0@gmail.com>
2015-10-03 21:36:46 +0100

    Updated location to use old API so no network connection

--- app/src/main/java/hackathon/london/tearfunddisasterresponse/LocationActivity.java
+++ app/src/main/java/hackathon/london/tearfunddisasterresponse/LocationActivity.java
@@ -1,131 +1,69 @@
 package hackathon.london.tearfunddisasterresponse;
 
-import android.content.IntentSender;
+import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
-import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+import java.util.Date;
 
-public class LocationActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
 
-    /*
-     * Define a request code to send to Google Play services
-     * This code is returned in Activity.onActivityResult
-     */
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+public class LocationActivity extends AppCompatActivity {
 
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
+    private static final String TAG = "LocationActivity";
+
+    // Acquire a reference to the system Location Manager
+    private LocationManager locationManager;
+
+    // Define a listener that responds to location updates
+    private LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            makeUseOfNewLocation(location);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d(TAG, "status changed, provider: " + provider + ", status: " + status);
+        }
+
+        public void onProviderEnabled(String provider) {
+            Log.d(TAG, "provider enabled");
+        }
+
+        public void onProviderDisabled(String provider) {
+            Log.d(TAG, "provider disabled");
+        }
+    };
+
+    private void makeUseOfNewLocation(Location location) {
+        locationManager.removeUpdates(locationListener);
+        Log.d(TAG, "making use of new location...");
+        // change screen state "got location!"
+        TextView locationStateTextView = (TextView) findViewById(R.id.locationState);
+        locationStateTextView.setText("Y");
+
+        // move to next app
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout lView = new LinearLayout(this);
+        setContentView(R.layout.activity_location);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+        Log.d(TAG, "onCreate");
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        // Create the LocationRequest object
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
-                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
-
-        System.out.println("No location yet.");
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, (long) 0, (float) 0, locationListener);
+        Log.d(TAG, "location updates requested");
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mGoogleApiClient.connect();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
 
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
-        }
-    }
 
-    private void closeActivity(){
-        Intent intent = new Intent(this, QuestionActivity.class);
-        this.startActivity(intent);
-    }
-
-    private void handleNewLocation(Location location) {
-        /*
-        output to dataclass location.toString();
-        */
-        System.out.println(location.toString());
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (location == null) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        }
-        else {
-            handleNewLocation(location);
-            closeActivity();
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Connection Failed.");
-            closeActivity();
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        handleNewLocation(location);
-        closeActivity();
-    }
 }
